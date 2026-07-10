d2cd97df0ffd8a2d8934168840778e11e1407498
Julito-Dev <julianpaez180@gmail.com>
2026-07-10 15:49:00 -0500

    feat: Methods that clasifies neutrons according to their speed.

--- include/Neutron.h
+++ include/Neutron.h
@@ -6,4 +6,17 @@ class Neutron : public Particle {
     public:
         Neutron( Vector2d pos, Vector2d vel):
             Particle(pos, vel, PhysicsConstants::NEUTRON_MASS) {}
+
+        float getSpeed() const {
+            return velocity.magnitude();
+        }
+
+        // 5.5 is the speed threshold for a neutral to be considered fast or not.
+        bool isFast() const {
+            return getSpeed() >= PhysicsConstants::FAST_THERMAL_THRESOLD;
+        }
+
+        bool isThermal() const {
+            return getSpeed() < PhysicsConstants::FAST_THERMAL_THRESOLD;
+        }
 };
