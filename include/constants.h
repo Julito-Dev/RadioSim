09ef167c39890261ceede7d65b8f372ebf651c1a
Julito-Dev <julianpaez180@gmail.com>
2026-07-10 18:59:40 -0500

    feat: Add a PI constant

--- include/constants.h
+++ include/constants.h
@@ -11,4 +11,6 @@ namespace PhysicsConstants {
 
     // 5.5 is the speed threshold for a neutral to be considered fast or not.
     constexpr float FAST_THERMAL_THRESOLD = (FAST_NEUTRON_SPEED + THERMAL_NEUTRON_SPEED) / 2;
+
+    constexpr float PI = 3.14159265358979323846f;
 }
