32575f98ae4efee4c0b183f5b34b9c847f164b1a
Julito-Dev <julianpaez180@gmail.com>
2026-07-09 20:49:50 -0500

    add: Class Vector

---
+++ include/Vector2d.h
@@ -0,0 +1,40 @@
+# pragma once
+# include <cmath>
+# include <iostream>
+
+class Vector2d {
+    public:
+        float x, y;
+
+    Vector2d(float x = 0.0f, float y = 0.0f) : x(x), y(y) {}
+
+    Vector2d operator+(const Vector2d& other) const {
+        return Vector2d(x + other.x, y + other.y);
+    }
+
+    Vector2d operator-(const Vector2d& other) const {
+        return Vector2d(x - other.x, y - other.y);
+    }
+
+    Vector2d operator*(float scalar) const {
+        return Vector2d(x * scalar, y * scalar);
+    }
+
+    Vector2d operator+=(const Vector2d& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    float magnitude() const {
+        return std::sqrt((x*x) + (y*y));
+    }
+
+    float distanceTo(const Vector2d& other) const {
+        return (*this - other).magnitude();
+    }
+
+    void print() const {
+        std::cout << "(" << x << ", " << y << ")";
+    }
+};
