1f0ae044eded40ae32a008bd6c6b0eab484cad7a
Julito-Dev <julianpaez180@gmail.com>
2026-07-10 15:43:09 -0500

    Typo: Error in constants.h

--- include/constants.h
+++ include/constants.h
@@ -7,5 +7,5 @@ namespace PhysicsConstants {
     // "Fast" neutron (just released by fision) are more faster.
     // than "thermal" neutrons (already stopped by the moderator)
     constexpr float FAST_NEUTRON_SPEED = 10.0f;
-    constexpr float THERMAL_NEUTRON_SPEDD = 1.0f;
+    constexpr float THERMAL_NEUTRON_SPEED = 1.0f;
 }
