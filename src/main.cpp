c3ad297595925e1d01d05d3320625d1920147851
Julito-Dev <julianpaez180@gmail.com>
2026-07-15 14:17:40 -0500

    feat: Main nows records a little simulation and save it into a json file

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,36 @@
 # include "../include/FuelAtom.h"
 # include "../include/constants.h"
 # include "../include/Reactor.h"
+# include "../include/simulationRecorder.h"
 
 int main(){
-    
+
+    std::cout << "Loading simulation...\n";
+
     Reactor reactor(42, 1.8f, 25.0f);
+    SimulationRecorder recorder;
 
     reactor.addNeutron(Neutron(Vector2d(0,0), Vector2d(7.0f, 5.0f)));
     reactor.addNeutron(Neutron(Vector2d(0,0), Vector2d(-5.0f, 8.0f)));
     reactor.addNeutron(Neutron(Vector2d(0,0), Vector2d(3.0f, -9.0f)));
 
     for (int x = -15; x <= 15; x++) {
-    for (int y = -15; y <= 15; y++) {
-        if ((x + y) % 2 == 0) {  // patrón tipo "tablero" para no saturar demasiado
-            reactor.addFuelAtom(FuelAtom(Vector2d(x, y), 0.85f, 0.85f, 3));
+        for (int y = -15; y <= 15; y++) {
+            if ((x + y) % 2 == 0) {
+                reactor.addFuelAtom(FuelAtom(Vector2d(x, y), 0.85f, 0.85f, 3));
+            }
         }
     }
-}
-    float deltatime = 0.2f;
 
-    for (int tick = 0; tick < 150; tick++){
+    float deltatime = 0.1f;   //increment this value to improve frames per second
+    int totalTicks = 1000;     //increment this value to improve frames per second
+
+    for (int tick = 0; tick < totalTicks; tick++){
         reactor.step(deltatime);
-        std::cout <<"Tick "<< tick
-                << " | Active Neutrons: " << reactor.getActiveNeutrons()
-                << " | Consumed Atoms: " << reactor.getConsumedAtomCount()
-                << " | Escaped Neutrons: "<< reactor.getEscapedCount()
-                << "\n";
-        reactor.printNeutronPositions();
+        recorder.captureFrame(tick, reactor);
     }
+
+    recorder.saveTofile("simulation.json");
+    std::system("python visualizer/main.py");
     return 0;
 }
